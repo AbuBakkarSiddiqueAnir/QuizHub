1de459b3048d1dcd9b72a812aa024aed8fdbddf9
AbuBakkarSiddiqueAnir <107474981+AbuBakkarSiddiqueAnir@users.noreply.github.com>
2023-03-25 01:46:21 +0600

    package: material ui integrated

--- client/client-nextjs/components/common/Navbar.tsx
+++ client/client-nextjs/components/common/Navbar.tsx
@@ -1,16 +1,122 @@
+import React from "react";
+import Link from "next/link";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  InputBase,
+  Button,
+} from "@material-ui/core";
+import { makeStyles, alpha } from "@material-ui/core/styles";
+import { Search as SearchIcon, Person as PersonIcon } from "@material-ui/icons";
 
-import React from 'react';
+interface NavbarProps {
+  logoSrc: string;
+}
 
-type NavbarProps = {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: "flex",
+    alignItems: "center",
+  },
+  logo: {
+    height: "30px",
+    marginRight: theme.spacing(1),
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+  button: {
+    marginLeft: theme.spacing(1),
+  },
+}));
 
+const Navbar: React.FC<NavbarProps> = ({ logoSrc }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar  color="#fff"  position="static">
+        <Toolbar>
+          {/* <img src={logoSrc} alt="Website logo" className={classes.logo} /> */}
+          <Typography variant="h6" className={classes.title}>
+            <Link href="/">
+              Byte Trivia
+            </Link>
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            startIcon={<PersonIcon />}
+          >
+            Sign In
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            className={classes.button}
+          >
+            Register
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
-const Navbar:React.FC<NavbarProps> = () => {
-
-    return (
-        <>
-
-        </>
-    )
-}
 export default Navbar;
--- client/client-nextjs/pages/_document.tsx
+++ client/client-nextjs/pages/_document.tsx
@@ -1,13 +1,39 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import { ServerStyleSheets } from "@material-ui/core/styles";
 
-export default function Document() {
+const MyDocument: React.FC = () => {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head/>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  // Render the app and collect the styles using Material UI's ServerStyleSheets
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  // Return the initial props along with the Material UI styles
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
+
+export default MyDocument;
